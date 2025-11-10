adfc9ab144d6b8905d94235531fefe334970f120
maax-yare <raagefx4@gmail.com>
2025-11-10 12:53:15 +0300

    Ecxercise-4

---
+++ Excercise-4/main.js
@@ -0,0 +1,28 @@
+console.log("Diclaration Function example")
+console.log(" ")
+
+
+function Add(Num1,Num2){
+    console.log("the addition of the two numbers are: " +(Num1+Num2))
+}
+
+Add(10,20)
+Add(40,20)
+Add(100,70)
+Add(1000,500)
+Add(10,2)
+
+console.log(" ")
+console.log("Expression Function example")
+console.log(" ")
+
+const Sum = function(Num1,Num2){
+     console.log("the addition of the two numbers are: " +(Num1+Num2))
+}
+
+
+Add(10,20)
+Add(40,20)
+Add(100,70)
+Add(1000,500)
+Add(10,2)
