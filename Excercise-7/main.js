76fbe0e442e557cb979a439f920c8289686b8204
maax-yare <raagefx4@gmail.com>
2025-11-12 12:24:31 +0300

    Excercise-7

---
+++ Excercise-7/main.js
@@ -0,0 +1,11 @@
+let Colors = ["red", "blue","white", "black", "yellow", "pink"]
+
+console.log(Colors)
+
+console.log("After push")
+Colors.push("gray")
+console.log(Colors)
+
+console.log("After shift")
+Colors.shift()
+console.log(Colors)
