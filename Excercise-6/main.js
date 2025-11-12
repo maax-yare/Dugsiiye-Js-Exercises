a7f2b937c15a6266b96ce63a42c30ef06a484c6e
maax-yare <raagefx4@gmail.com>
2025-11-12 11:42:45 +0300

    Excercise-6

---
+++ Excercise-6/main.js
@@ -0,0 +1,9 @@
+let Colors = ["red", "blue","white", "black", "yellow", "pink"]
+
+console.log(Colors)
+console.log(Colors[0])
+console.log(Colors[1])
+console.log(Colors[2])
+console.log(Colors[3])
+console.log(Colors[4])
+console.log(Colors[5])
