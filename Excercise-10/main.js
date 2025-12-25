a21d5aa6f5b0ed385153d402772859cdfe0e0a74
maax-yare <raagefx4@gmail.com>
2025-12-25 11:12:31 +0300

    Excercise-10

---
+++ Excercise-10/main.js
@@ -0,0 +1,22 @@
+
+let Library = [
+    {
+        title: "And Then There Were None",
+        author: "Agatha Christie",
+        year: 1939
+    },
+    {
+        title: "Demon Copperhead",
+        author: "Barbara Kingsolver",
+        year: 2022
+    },
+    {
+        title: "A Tale of Two Cities",
+        author: "Charles Dickens",
+        year: 1859
+    }
+
+];
+
+console.log(Library[0].title);
+console.log(Library[1].author);
