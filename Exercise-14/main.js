46e846993fd3559edfa6a2e373c62d66568efb3e
maax-yare <raagefx4@gmail.com>
2025-12-25 12:58:45 +0300

    Exercise-14

---
+++ Exercise-14/main.js
@@ -0,0 +1,9 @@
+let password;
+
+do {
+    password = prompt("Enter your password!");
+} while (password !== "10"); // Loop wuu soconaya inta password ka la galiyay la midyahay "10"
+
+console.log("welcome legend");
+
+    
