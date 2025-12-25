1309d4b0cedc0a891e109b99f666ff2b6b035661
maax-yare <raagefx4@gmail.com>
2025-12-25 10:43:32 +0300

    Excercise-9

---
+++ Excercise-9/main.js
@@ -0,0 +1,17 @@
+
+let car = {
+    make : "SYV",
+    model : "Model S",
+    year: "2025",
+    Start: function() {
+        console.log("the car has started.")
+    }
+};
+
+car.status = "are you ready!"
+
+console.log(car.make),
+console.log(car.model),
+console.log(car.year),
+console.log(car.status),
+console.log(car.Start())
