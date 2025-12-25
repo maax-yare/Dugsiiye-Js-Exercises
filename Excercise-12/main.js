f5a81229e3d9ab2546ac7fad465615a952bae829
maax-yare <raagefx4@gmail.com>
2025-12-25 12:07:46 +0300

    Excercise-12

---
+++ Excercise-12/main.js
@@ -0,0 +1,25 @@
+const fruits = [
+  "Apple",
+  "Banana",
+  "Cherry",
+  "Date",
+  "Elderberry",
+  "Fig",
+  "Grape",
+  "Honeydew",
+  "Kiwi",
+  "Lemon",
+  "Mango",
+  "Nectarine",
+  "Orange",
+  "Papaya",
+  "Quince",
+  "Raspberry",
+  "Strawberry",
+  "Tangerine",
+  "Watermelon"
+];
+
+for (let i=0; i < fruits.length; i++) {
+    console.log(fruits[i])
+}
