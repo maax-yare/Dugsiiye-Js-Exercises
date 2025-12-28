57544c1f127bd49554c12f1f8a77277fad6c1195
maax-yare <raagefx4@gmail.com>
2025-12-28 13:08:42 +0300

    Exercise-17

---
+++ Exercise-17/main.js
@@ -0,0 +1,12 @@
+
+let temperature = -14;
+
+if (temperature < 0){
+    console.log("Very cold");
+}else if(temperature < 15){
+    console.log("Cold");
+}else if(temperature < 25){
+    console.log("Warm");
+}else {
+    console.log("Hot");
+}
